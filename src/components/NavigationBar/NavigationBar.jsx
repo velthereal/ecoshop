03d2e7eca0bf6d463f3b5af867de3132e70fb0e7
velthereal <lera2005olex@gmail.com>
2024-01-04 17:01:39 +0200

    finished Navigation Bar

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -1,10 +1,24 @@
-// import styles from './navigationbar.css';
+import styles from './navigationbar.module.css';
 
 import SerachBar from '../SearchBar';
+import PageWrapper from '../PageWrapper';
+
+import logo from '../../images/logo/Logo.png';
+import profile from '../../images/icons/profile.png';
+import bag from '../../images/icons/bag.png';
 
 const NavigationBar = () => {
 	return (
-		<SerachBar />
+		<PageWrapper>
+			<div className={styles['navigation-bar']}>
+				<img src={logo} alt='EcoShop Logo'/>
+				<SerachBar />
+				<div className={styles.icons}>
+					<img src={profile} alt="Profile icon" />
+					<img src={bag} alt="Shopping icon" />
+				</div>
+			</div>
+		</PageWrapper>
 	)
 }
 
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,6 +1,14 @@
-// import styles from './searchbar.css';
+import styles from './searchbar.module.css';
+
+import search from '../../images/icons/search.png'
 
 const SerachBar = () => {
+	return (
+		<div className={styles['serach-bar']}>
+			<img className={styles.search} src={search} alt="" />
+			<input type="serach" placeholder='Search here...' />
+		</div>
+	)
 
 }
 
